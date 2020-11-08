c7fb1590b13e86c80c77e0d945a20cc4d36f5ccb
Sagar Gupta <sgrkmr12@gmail.com>
2020-11-08 05:46:21 +0530

    minor Code refactor

--- app/src/main/java/com/showcase/highlightstoday/Defaults.kt
+++ app/src/main/java/com/showcase/highlightstoday/Defaults.kt
@@ -1,7 +1,7 @@
 package com.showcase.highlightstoday
 
 const val DEFAULT_TAG = "Technology"
-
+const val API_KEY = BuildConfig.API_KEY
 val TAG_LIST = listOf(
     "General",
     DEFAULT_TAG,
--- app/src/main/java/com/showcase/highlightstoday/repository/database/room/AppDatabase.kt
+++ app/src/main/java/com/showcase/highlightstoday/repository/database/room/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.showcase.highlightstoday.repository.database.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ArticleDbo::class], version = 1)
+@Database(entities = [ArticleDbo::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun getArticleDao(): ArticleDao
 }
